c4cffe5aa0cb6c4e75c79fc1ea74d5d39700a967
basmalayasser19 <basmalayasser19@gmail.com>
2026-03-29 12:23:56 +0200

    Task 03 (3.2)

--- Online Store Adv C#/Program.cs
+++ Online Store Adv C#/Program.cs
@@ -114,6 +114,56 @@
         //} 
         #endregion
 
+        #region Task 03 : (3.2 Transform Products)
+        //public static List<TResult> TransformProducts<TResult>(List<Product> products, Func<Product, TResult> transformer)
+        //{
+        //    List<TResult> result = new List<TResult>();
+
+        //    foreach (var product in products)
+        //    {
+        //        result.Add(transformer(product));
+        //    }
+
+        //    return result;
+        //}
+
+        //static void Main(string[] args)
+        //{
+        //    // Product Catalog
+        //    List<Product> catalog = new List<Product>
+        //        {
+        //            new Product { Id = 1, Name = "Laptop", Category = "Electronics", Price = 1200, Stock = 10 },
+        //            new Product { Id = 2, Name = "Phone", Category = "Electronics", Price = 800, Stock = 25 },
+        //            new Product { Id = 3, Name = "T-Shirt", Category = "Clothing", Price = 30, Stock = 100 },
+        //            new Product { Id = 4, Name = "Jeans", Category = "Clothing", Price = 60, Stock = 50 },
+        //            new Product { Id = 5, Name = "Food", Category = "Clothing", Price = 5, Stock = 200 },
+        //            new Product { Id = 6, Name = "Coffe Beans", Category = "Food", Price = 15, Stock = 80 },
+        //            new Product { Id = 7, Name = "C# Book", Category = "Books", Price = 45, Stock = 30 },
+        //            new Product { Id = 8, Name = "Novel", Category = "Books", Price = 20, Stock = 60 },
+        //            new Product { Id = 9, Name = "Headphones", Category = "Electronics", Price = 150, Stock = 40 },
+        //            new Product { Id = 10, Name = "Jacket", Category = "Clothing", Price = 120, Stock = 15 }
+        //        };
+
+        //    // Scenario 3
+        //    Console.WriteLine("=== Summary List ===");
+        //    var summary = TransformProducts(catalog, p => $"{p.Name} (${p.Price})");
+
+        //    foreach (var item in summary)
+        //    {
+        //        Console.WriteLine(item);
+        //    }
+
+        //    // Scenario 4
+        //    Console.WriteLine("\n=== Price Labels ===");
+        //    var labels = TransformProducts(catalog, p =>
+        //        p.Price > 100 ? "Expensive!" : "Affordable");
+
+        //    for (int i = 0; i < catalog.Count; i++)
+        //    {
+        //        Console.WriteLine($"{catalog[i].Name}: {labels[i]}");
+        //    }
+        //} 
+        #endregion
     }
 
 
