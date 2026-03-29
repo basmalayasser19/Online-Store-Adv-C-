927324e89a14848ea85599e1056346f468f2a436
basmalayasser19 <basmalayasser19@gmail.com>
2026-03-29 12:29:08 +0200

    Task 03 (3.3)

--- Online Store Adv C#/Program.cs
+++ Online Store Adv C#/Program.cs
@@ -164,6 +164,53 @@
         //    }
         //} 
         #endregion
+
+        #region Task 03 : (3.3 Filter Products)
+        //public static List<Product> FilterProducts(List<Product> products, Predicate<Product> condition)
+        //{
+        //    List<Product> result = new List<Product>();
+
+        //    foreach (var product in products)
+        //    {
+        //        if (condition(product))
+        //        {
+        //            result.Add(product);
+        //        }
+        //    }
+
+        //    return result;
+        //}
+
+        //static void Main(string[] args)
+        //{
+        //    // Product Catalog
+        //    List<Product> catalog = new List<Product>
+        //            {
+        //                new Product { Id = 1, Name = "Laptop", Category = "Electronics", Price = 1200, Stock = 10 },
+        //                new Product { Id = 2, Name = "Phone", Category = "Electronics", Price = 800, Stock = 25 },
+        //                new Product { Id = 3, Name = "T-Shirt", Category = "Clothing", Price = 30, Stock = 100 },
+        //                new Product { Id = 4, Name = "Jeans", Category = "Clothing", Price = 60, Stock = 50 },
+        //                new Product { Id = 5, Name = "Food", Category = "Clothing", Price = 5, Stock = 200 },
+        //                new Product { Id = 6, Name = "Coffe Beans", Category = "Food", Price = 15, Stock = 80 },
+        //                new Product { Id = 7, Name = "C# Book", Category = "Books", Price = 45, Stock = 30 },
+        //                new Product { Id = 8, Name = "Novel", Category = "Books", Price = 20, Stock = 60 },
+        //                new Product { Id = 9, Name = "Headphones", Category = "Electronics", Price = 150, Stock = 40 },
+        //                new Product { Id = 10, Name = "Jacket", Category = "Clothing", Price = 120, Stock = 15 }
+        //            };
+
+        //    Console.WriteLine("=== Low Stock Alert ===");
+
+        //    var lowStockProducts = FilterProducts(catalog, p => p.Stock < 20);
+
+        //    foreach (var p in lowStockProducts)
+        //    {
+        //        Console.WriteLine($"[LOW STOCK] {p.Name}: only {p.Stock} left!");
+        //    }
+
+        //} 
+        #endregion
+
+
     }
 
 
